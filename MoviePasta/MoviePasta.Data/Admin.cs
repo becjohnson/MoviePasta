52288b989c28805963b3e82247681ec197253ed8
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-12-02 13:17:56 -0500

    Added Comment

---
+++ MoviePasta/MoviePasta.Data/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviePasta.Data
+{
+    public class Admin
+    {
+        public int AdminId { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
