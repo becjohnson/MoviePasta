81aae3495f0e6d45784db029a8e8b4dd83d553f0
Rebecca Johnson <91417720+becjohnson@users.noreply.github.com>
2021-12-02 13:37:08 -0500

    Update StarRating.cs

--- MoviePasta/MoviePasta.Data/StarRating.cs
+++ MoviePasta/MoviePasta.Data/StarRating.cs
@@ -11,15 +11,20 @@ namespace MoviePasta.Data
     {
         [Key]
         public int RatingId { get; set; }
-
+        [ForeignKey(nameof(MovieId)]
+        public int MovieId { get; set; }
+        [ForeignKey(nameof(TvId)]
+        public int TvId { get; set; }
         [Required]
         public double NumberStars { get; set; }
+        [Required]
         public double AverageRating { get; set; }
+        [Required]
         public string MovieName { get; set; }
+        [Required]
         public string MovieDescription { get; set; }
         [Required]
         [MaxLength(4000)]
         public string Comment { get; set; }
-
     }
 }
