0aa8710dd282e308016fe1452f7caf2cf7c340ba
Rebecca Johnson <91417720+becjohnson@users.noreply.github.com>
2021-12-02 12:17:49 -0500

    Update IdentityModels.cs

--- MoviePasta/MoviePasta.Data/IdentityModels.cs
+++ MoviePasta/MoviePasta.Data/IdentityModels.cs
@@ -30,4 +30,29 @@ namespace MoviePasta.WebAPI.Models
             return new ApplicationDbContext();
         }
     }
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
 }
